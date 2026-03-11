7d03d30bf014b9693edd465d66e525efdc3ed8e9
Paul <udegbuepaul43@gmail.com>
2026-03-11 15:13:26 +0100

    New C# assesemt done

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Task 1
+        string name = "John Doe";
+        int age = 25;
+        bool isAdmin = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Is Admin: " + isAdmin);
+
+        Console.WriteLine();
+
+        // Task 2
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+
+        Console.WriteLine();
+
+        // Task 3
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine();
+
+        // Task 4
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        int sum = 0;
+
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+            sum += num;
+        }
+
+        Console.WriteLine("Sum: " + sum);
+
+        Console.WriteLine();
+
+        // Task 5
+        Greet("Alice");
+    }
+
+    static void Greet(string name)
+    {
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
